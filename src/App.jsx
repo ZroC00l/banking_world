f3d7738d64063475d10119955c019876c42e26e5
ZroC00l <giftgabanakgosi@gmail.com>
2022-08-29 14:43:55 +0200

    updated the tailwind packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import React from "react";
 import styles from "./styles";
+import {
+  Navbar,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Testimonials,
+  Stats,
+  Hero,
+  GetStarted,
+  Footer,
+} from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     {/*Div to wrap our navbar */}
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>{/*Navbar Component  */}</div>
+      <div className={`${styles.boxWidth}`}>
+        {/*Navbar Component  */}
+        <Navbar />
+      </div>
     </div>
 
     {/* Hero Section*/}
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>{/*Feature Hero Component */}</div>
+      <div className={`${styles.boxWidth}`}>
+        {/*Feature Hero Component */}
+        <Hero />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         {/*Feature Stats,Business,Billing,CardDeal,Testimonials,Clients,CTA,Footer Component */}
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Button from "./Button";
+import Stats from "./Stats";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Business from "./Business";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Button,
+  Stats,
+  Clients,
+  CTA,
+  Testimonials,
+  Business,
+  FeedbackCard,
+  Footer,
+  Hero,
+  GetStarted,
+};
