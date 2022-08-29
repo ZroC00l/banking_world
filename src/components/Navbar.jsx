824d98c7bd32b6b6c3da221fad23d42a7f8b13d1
ZroC00l <giftgabanakgosi@gmail.com>
2022-08-29 15:53:19 +0200

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="banking-world" className="w-[124px] h-[32px]" />
+      {/*Navbar styling for sm devices */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white `}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      {/*Navbar for Desktop devices */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggleMenu ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggleMenu((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggleMenu ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        ></div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
