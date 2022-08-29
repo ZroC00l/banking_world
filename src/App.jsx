f1ef8cef375202a7e0170945b1bfc8ed2d8edf53
ZroC00l <giftgabanakgosi@gmail.com>
2022-08-29 14:07:13 +0200

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./styles";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/*Div to wrap our navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>{/*Navbar Component  */}</div>
+    </div>
+
+    {/* Hero Section*/}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>{/*Feature Hero Component */}</div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/*Feature Stats,Business,Billing,CardDeal,Testimonials,Clients,CTA,Footer Component */}
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
