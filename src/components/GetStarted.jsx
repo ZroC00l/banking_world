2c7a7afd9bc012e37f0c9f8ebc2a486f2bf472a9
ZroC00l <giftgabanakgosi@gmail.com>
2022-08-30 11:52:38 +0200

    hero component banner image section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -6,7 +6,7 @@ const GetStarted = () => (
     className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
   >
     <div
-      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+      className={`${styles.flexCenter} flex-col bg-primary w-[98%] h-[98%] rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
