7f0a26563244b6806ff78600b010d7985622e5a4
ZroC00l <giftgabanakgosi@gmail.com>
2022-09-01 09:15:21 +0200

    card deal component done, tesitimonials section update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { feedback } from "../constants";
+import styles from "../styles";
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => {
   return <div>Testimonials</div>;
